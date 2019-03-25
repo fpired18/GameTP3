900f5028290a458d50b92f4dc6e59b7314b41e1c
etudiant-13 <fpired@icloud.com>
2019-03-25 11:15:27 +0100

    Création du projet 3

---
+++ GameTp3/GameTp3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GameTp3
+//
+//  Created by etudiant-13 on 25/03/2019.
+//  Copyright © 2019 fpired. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
